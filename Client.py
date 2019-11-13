7fb54a5b1e2ed2f0b5226b9a704d46008a9128cc
Andreea Condrea <52072133+andreeacondrea@users.noreply.github.com>
2019-11-13 23:08:08 +0200

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,25 @@
+import socket                   # Import socket module
+
+s = socket.socket()             # Create a socket object
+host = '192.168.1.3'  #Ip address that the TCPServer  is there
+port = 50000                     # Reserve a port for your service every new transfer wants a new port or you must wait.
+
+s.connect((host, port))
+s.send("Hello server!")
+
+with open('received_file', 'wb') as f:
+    print 'file opened'
+    while True:
+        print('receiving data...')
+        data = s.recv(1024)
+        print('data=%s', (data))
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
+s.send('Cu pacere fata draga')
+f.close()
+print('Successfully get the file')
+s.close()
+print('connection closed')
+
